b0258855b40c97d184209453487781de575af661
sharvari-pisal <sharvaripisal02@gmail.com>
2024-01-17 21:50:01 +0530

    Updated files

--- Task 1 Number Game/Numbergame.java
+++ Task 1 Number Game/Numbergame.java
@@ -5,11 +5,9 @@ public class Numbergame {
     public static void main(String[] args) {
         playNumbergame();
     }
-
     private static void playNumbergame() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
         int minRange = 1;
         int maxRange = 100;
         int maxAttempts = 10;
@@ -22,7 +20,6 @@ public class Numbergame {
             
             int secretNumber = random.nextInt(maxRange - minRange + 1) + minRange;
 
-            
             int attempts = 0;
 
             System.out.println("\nGuess the number between " + minRange + " and " + maxRange + "!");
@@ -32,7 +29,6 @@ public class Numbergame {
                 System.out.print("Enter your guess: ");
                 int userGuess = scanner.nextInt();
 
-                
                 if (userGuess == secretNumber) {
                     System.out.println("Congratulations! You guessed the number " + secretNumber + " correctly!");
                     roundsWon++;
@@ -55,7 +51,6 @@ public class Numbergame {
         System.out.println("\nGame over! \nYour total score is " + roundsWon +
                 " rounds won with an average of " + (double) totalAttempts / roundsWon + " attempts per round.");
 
-        
         scanner.close();
     }
 }
